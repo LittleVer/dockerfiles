cbc31485d08774273e29afc620f72ff5209c1462
limaofeng <limaofeng@msn.com>
2016-10-07 22:08:26 +0800

    Update entrypoint.sh

--- gitlab-sonar-scanner/entrypoint.sh
+++ gitlab-sonar-scanner/entrypoint.sh
@@ -10,7 +10,7 @@ OPTS="-Dsonar.gitlab.project_id=${CI_PROJECT_ID} -Dsonar.gitlab.commit_sha=${CI_
 # TODO: Improve entrypoint to support gitlab-runner
 cd ${CI_PROJECT_DIR}
 if [[ ! -z $SONAR_TOKEN ]]; then
-  ${SONAR_SCANNER_HOME}/bin/sonar-scanner -Dsonar.host.url=${SONAR_HOST} -Dsonar.login=${SONAR_TOKEN} -Dsonar.projectKey=${PROJECT} -Dsonar.projectName=${PROJECT} -Dsonar.projectVersion=${VERSION} -Dsonar.sources=/workspace ${OPTS}
+  ${SONAR_SCANNER_HOME}/bin/sonar-scanner -Dsonar.host.url=${SONAR_HOST} -Dsonar.login=${SONAR_TOKEN} -Dsonar.projectKey=${PROJECT} -Dsonar.projectName=${PROJECT} -Dsonar.projectVersion=${VERSION} -Dsonar.sources=${CI_PROJECT_DIR} ${OPTS}
 else
   ${SONAR_SCANNER_HOME}/bin/sonar-scanner ${OPTS}
 fi
