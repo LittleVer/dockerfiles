bbb8127d71e210545472ea50b2568f46681622e1
李茂峰 <limaofeng@msn.com>
2016-12-12 12:40:20 +0800

    修改

--- owncloud/docker-entrypoint.sh
+++ owncloud/docker-entrypoint.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 set -e
 
-# ocdownloader requirements
-su - aria2 -c '/usr/bin/aria2c --enable-rpc --rpc-allow-origin-all -c -D --log=/dev/stdout --check-certificate=false &'
+if [ ! -e '/var/www/html/version.php' ]; then
+	tar cf - --one-file-system -C /usr/src/owncloud . | tar xf -
+    chown -R www-data /var/www/html
 
-# makes aria2 and www-data works together
-umask 0007
-apache2-foreground
+    # Make not existing ./data/ for specified permission
+	mkdir /var/www/html/data
+    useradd aria2
+    chown aria2:aria2 /var/www/html/data
+    chmod 770 /var/www/html/data
+    usermod -aG aria2 www-data
+    usermod -aG www-data aria2
+
+    # ocdownloader requirements
+    su - aria2 -c '/usr/bin/aria2c --enable-rpc --rpc-allow-origin-all -c -D --log=/dev/stdout --check-certificate=false &'
+
+    # makes aria2 and www-data works together
+    umask 0007
+fi
+
+exec "$@"
